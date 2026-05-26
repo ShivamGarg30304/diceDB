4d0dead7574acf1d05fda457d2dcd7cf777bb8a4
ShivamGarg30304 <shivamgarg30304@gmail.com>
2026-05-26 21:51:34 +0530

    Run Echo Server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"flag"
+	"log"
+
+	"github.com/shivam30303/diceDB/config"
+	"github.com/shivam30303/diceDB/server"
+)
+
+func setupFlags() {
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the dice server")
+	flag.IntVar(&config.Port, "port", 7379, "port of the dice server")
+	flag.Parse()
+}
+
 func main() {
-	
+	setupFlags()
+	log.Println("rolling the dice 🎲")
+	server.RunSyncTCPServer()
 }
