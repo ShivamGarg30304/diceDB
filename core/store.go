cbd98735344db834301cb29b00653acf23cd367d
ShivamGarg30304 <shivamgarg30304@gmail.com>
2026-06-06 23:23:35 +0530

    Added first key insert eviction

--- core/store.go
+++ core/store.go
@@ -1,6 +1,10 @@
 package core
 
-import "time"
+import (
+	"time"
+
+	"github.com/shivam30303/diceDB/config"
+)
 
 type Obj struct {
 	Value     interface{}
@@ -26,6 +30,9 @@ func NewObj(value interface{}, durationMs int64) *Obj {
 }
 
 func Put(k string, obj *Obj) {
+	if len(store) >= config.KeysLimit {
+		evict()
+	}
 	store[k] = obj
 }
 
