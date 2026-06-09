72d838aba03c7bcddbc442152a24ca0ba1aa5a00
ShivamGarg30304 <shivamgarg30304@gmail.com>
2026-06-09 23:12:32 +0530

    Implement Graceful Termination

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/shivam30303/diceDB/config"
 	"github.com/shivam30303/diceDB/server"
@@ -17,6 +21,14 @@ func setupFlags() {
 func main() {
 	setupFlags()
 	log.Println("rolling the dice 🎲")
-	err := server.RunAsyncTCPServer()
-	log.Printf("error : %v", err)
+
+	var sigs chan os.Signal = make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go server.RunAsyncTCPServer()
+	go server.WaitForSignal(&wg, sigs)
+
+	wg.Wait()
 }
